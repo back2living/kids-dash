ea7dc0c760491f471f43068e7ba9f95a56b44486
abiolasikal <sikal.sikal.ss@gmail.com>
2024-08-28 22:12:43 +0100

    feat/api(): app updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import {Toaster} from "react-hot-toast";
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import {useState} from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(() => new QueryClient());
+
+  return <QueryClientProvider client={queryClient}>
+    <Toaster/>
+    <Component {...pageProps} />
+  </QueryClientProvider>;
 }
