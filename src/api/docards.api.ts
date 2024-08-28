9fea6605ae9eb659c24cfc5078b79cfb311ddbe6
abiolasikal <sikal.sikal.ss@gmail.com>
2024-08-28 21:40:14 +0100

    feat/api(): docards api

---
+++ src/api/docards.api.ts
@@ -0,0 +1,40 @@
+import instance, {baseUrl} from "@/api/instance";
+import {IUpdateStorefrontDoCard} from "@/api/storefront.api";
+
+export interface IAddDoCard {
+    points: number;
+    isMandatory?: boolean;
+    purpose: string;
+    storefrontItemId?: string;
+    type: string;
+    avatar?:string;
+}
+
+export interface IDoCardPayment {
+    doCardId: string;
+    points: number;
+    status?: string;
+    type?: string;
+    isMandatory?: boolean;
+}
+
+export const fetchDoCards = async (status: string, type="", isMandatory?: boolean) => {
+    const {data} = await instance.get(`${baseUrl}/docards?status=${status}&type=${type}&isMandatory=${isMandatory}`);
+    return data;
+}
+export const addDoCard = async ({storefrontItemId, purpose, points, isMandatory, type, avatar}: IAddDoCard) => {
+    const {data} = await instance.post(`${baseUrl}/docards/kid`, {isMandatory, purpose, type, points, storefront: storefrontItemId, avatar});
+    return data;
+}
+export const updateDoCard = async ({isMandatory, purpose, type, points, doCardId}: IUpdateStorefrontDoCard) => {
+    const {data} = await instance.put(`${baseUrl}/docards/docard/${doCardId}`, {isMandatory, purpose, type, points});
+    return data;
+}
+export const addDoCardPayment = async ({doCardId, points}: IDoCardPayment) => {
+    const {data} = await instance.post(`${baseUrl}/docards/docard/${doCardId}/payment`, {points});
+    return data;
+}
+export const deleteDoCard = async ({doCardId}: { doCardId: string }) => {
+    const {data} = await instance.delete(`${baseUrl}/docards/docard/${doCardId}`);
+    return data;
+}
