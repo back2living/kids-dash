abe9193c869b95368a00248bc9d0a4af7e12e254
abiolasikal <sikal.sikal.ss@gmail.com>
2024-08-28 21:40:03 +0100

    feat/api(): useDocards hooks

---
+++ src/hooks/useDocards.ts
@@ -0,0 +1,73 @@
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
+import {
+    addDoCard, addDoCardPayment,
+    deleteDoCard,
+    fetchDoCards,
+    IAddDoCard, IDoCardPayment,
+    updateDoCard
+} from "@/api/docards.api";
+import {
+    IUpdateStorefrontDoCard
+} from "@/api/storefront.api";
+import toast from "react-hot-toast";
+
+export const useFetchKidDoCards = (status: string, type: string, isMandatory?: boolean) => {
+    return useQuery({
+        queryKey: ["allDoCards", status, type, isMandatory],
+        queryFn: () => fetchDoCards(status, type, isMandatory)
+    });
+}
+export const useAddDoCardPayment = (closeModal: () => void) => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: ({doCardId, points}: IDoCardPayment) => addDoCardPayment({doCardId, points}),
+        onSuccess: async (data, variables) => {
+            await queryClient.invalidateQueries({ queryKey: ["allDoCards", variables.status, variables.type, variables.isMandatory] });
+            await queryClient.invalidateQueries({ queryKey: ["allDoCards"] });
+            toast.success(data?.message);
+            closeModal();
+        },
+        onError: (error: any) =>  toast.error(error?.response?.data?.message)
+    })
+}
+export const useAddDoCard = (handleRouteToKidProfile: () => void) => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: ({storefrontItemId, points, type, purpose, isMandatory, avatar}: IAddDoCard) => addDoCard({storefrontItemId, purpose, points, isMandatory, type, avatar}),
+        onSuccess: async (data) => {
+            handleRouteToKidProfile();
+            await queryClient.invalidateQueries({ queryKey: ["allDoCards"] });
+            toast.success(data?.message);
+        },
+        onError: (error: any) =>  toast.error(error?.response?.data?.message)
+    })
+}
+
+export const useUpdateDoCard = (closeModal: () => void) => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: ({doCardId, points, type, purpose, isMandatory}: IUpdateStorefrontDoCard) => updateDoCard({isMandatory, purpose, type, points, doCardId}),
+        onSuccess: async (data, variables) => {
+            await queryClient.invalidateQueries({ queryKey: ["allDoCards"] });
+            toast.success("Category added successfully.");
+            closeModal();
+        },
+        onError: (error: any) =>  toast.error(error?.response?.data?.message)
+    })
+}
+export const useDeleteDoCard = (closeModal: () => void) => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: ({doCardId}: { doCardId: string }) => deleteDoCard({doCardId}),
+        onSuccess: async (data, variables) => {
+            await queryClient.invalidateQueries({ queryKey: ["allDoCards"] });
+            toast.success("Category added successfully.");
+            closeModal();
+        },
+        onError: (error: any) =>  toast.error(error?.response?.data?.message)
+    })
+}
