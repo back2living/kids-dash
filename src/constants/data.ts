051e4f10eb1a882029bf21e585b49dc71e5d1016
abiolasikal <sikal.sikal.ss@gmail.com>
2024-08-28 21:56:03 +0100

    feat/api(): dummy data

---
+++ src/constants/data.ts
@@ -0,0 +1,386 @@
+import {
+    DashboardIcon,
+    ShopIcon,
+    SettingsIcon,
+    DoCardsIcon
+} from "@/components/shared/Svg";
+
+export const sidebarLinks = [
+    {
+        name: "Dashboard",
+        path: "/dashboard",
+        icon: DashboardIcon
+    },
+    {
+        name: "Do Cards",
+        path: "/do-cards",
+        icon: DoCardsIcon
+    },
+    {
+        name: "Storefront",
+        path: "/store",
+        icon: ShopIcon
+    },
+    {
+        name: "Settings",
+        path: "/settings",
+        icon: SettingsIcon
+    },
+];
+export const kidsData = [
+    {
+        name: "Layla Adams",
+        img: "/assets/images/boy-smile.webp",
+        balance: 2000,
+        points: 1000,
+        id: "12348593"
+    },
+    {
+        name: "Jane Adams",
+        img: "/assets/images/girl-smile.webp",
+        balance: 3000,
+        points: 1400,
+        id: "56348593"
+    },
+    {
+        name: "Wesley Adams",
+        img: "/assets/images/indian-girl.webp",
+        balance: 3000,
+        points: 1400,
+        id: "78948593"
+    },
+    {
+        name: "Layla Adams",
+        img: "/assets/images/smile-girl.webp",
+        balance: 2000,
+        points: 1000,
+        id: "24948593"
+    },
+    {
+        name: "Jane Adams",
+        img: "/assets/images/layla.png",
+        balance: 3000,
+        points: 1400,
+        id: "09848593"
+    },
+    {
+        name: "Wesley Adams",
+        img: "/assets/images/corporate-man.png",
+        balance: 3000,
+        points: 1400,
+        id: "98823593"
+    },
+    {
+        name: "Layla Adams",
+        img: "/assets/images/layla.png",
+        balance: 2000,
+        points: 1000,
+        id: "23148593"
+    },
+    {
+        name: "Jane Adams",
+        img: "/assets/images/layla.png",
+        balance: 3000,
+        points: 1400,
+        id: "56748593"
+    },
+    {
+        name: "Wesley Adams",
+        img: "/assets/images/corporate-man.png",
+        balance: 3000,
+        points: 1400,
+        id: "09048593"
+    }
+];
+export const tasks = ["🌞 Morning tasks", "📘 School work", "🏡 house work", "🧎🏻‍♂️ Ethics & values", "🌒 Night tasks"];
+export const shopData = [
+    {
+        name: "Sneakers",
+        img: "/assets/images/airforce.webp",
+        amount: 2000,
+        description: "UrbanStride sneakers are as comfy as clouds and as cool as can be! Perfect for all your adventures"
+    },
+    {
+        name: "Bag",
+        img: "/assets/images/bag.webp",
+        amount: 3000,
+        description: "UrbanStride sneakers are as comfy as clouds and as cool as can be! Perfect for all your adventures"
+    },
+    {
+        name: "Guitar",
+        img: "/assets/images/basketball.webp",
+        amount: 3000,
+        description: "UrbanStride sneakers are as comfy as clouds and as cool as can be! Perfect for all your adventures"
+    },
+    {
+        name: "Necklace",
+        img: "/assets/images/bike.webp",
+        amount: 2000,
+        description: "UrbanStride sneakers are as comfy as clouds and as cool as can be! Perfect for all your adventures"
+    },
+    {
+        name: "Boombox",
+        img: "/assets/images/boombox.webp",
+        amount: 3500,
+        description: "UrbanStride sneakers are as comfy as clouds and as cool as can be! Perfect for all your adventures"
+    },
+    {
+        name: "Ball",
+        img: "/assets/images/camera-one.webp",
+        amount: 1400,
+        description: "UrbanStride sneakers are as comfy as clouds and as cool as can be! Perfect for all your adventures"
+    },
+    {
+        name: "Bicycle",
+        img: "/assets/images/guitar.webp",
+        amount: 2000,
+        description: "UrbanStride sneakers are as comfy as clouds and as cool as can be! Perfect for all your adventures"
+    },
+    {
+        name: "Camera",
+        img: "/assets/images/guitar-one.webp",
+        amount: 3000,
+        description: "UrbanStride sneakers are as comfy as clouds and as cool as can be! Perfect for all your adventures"
+    },
+    {
+        name: "Boom box",
+        img: "/assets/images/necklace.webp",
+        amount: 3000,
+        description: "UrbanStride sneakers are as comfy as clouds and as cool as can be! Perfect for all your adventures"
+    },
+];
+export const savingsData =  [
+    {
+        name: "Buy a Bicycle",
+        itemImg: "/assets/images/bike.webp",
+        points: 300,
+        target: 400,
+        profileImage: "/assets/images/boy-smile.webp"
+    },
+    {
+        name: "December Shoes",
+        itemImg: "/assets/images/airforce.webp",
+        points: 150,
+        target: 400,
+        profileImage: "/assets/images/boy-smile.webp"
+    },
+    {
+        name: "Buy a Bag",
+        itemImg: "/assets/images/bag.webp",
+        points: 200,
+        target: 500,
+        profileImage: "/assets/images/girl-smile.webp"
+    },
+    {
+        name: "Buy a Guitar",
+        itemImg: "/assets/images/guitar.webp",
+        points: 270,
+        target: 600,
+        profileImage: "/assets/images/boy-smile.webp"
+    },
+];
+export const completedSavingsData =  [
+    {
+        name: "December Shoes",
+        itemImg: "/assets/images/airforce.webp",
+        points: 400,
+        target: 400,
+        profileImage: "/assets/images/indian-girl.webp"
+    },
+    {
+        name: "Buy a Guitar",
+        itemImg: "/assets/images/guitar.webp",
+        points: 300,
+        target: 300,
+        profileImage: "/assets/images/smile-girl.webp"
+    },
+
+];
+export const notificationsData = [
+    {name: "Nolan", heading: "Task completed", message: "Nolan has marked the task '🍪 Eat breakfast' as completed. ", read: false, img: "/assets/images/boy-smile.webp"},
+    {name: "Grey", heading: "Task completed", message: "Grey has marked the task '🍪 Eat breakfast' as completed. ", read: false, img: "/assets/images/boy-smile.webp"},
+    {name: "Toni", heading: "Task completed", message: "Toni has marked the task '🍪 Eat breakfast' as completed. ", read: false, img: "/assets/images/boy-smile.webp"},
+    {name: "Wesley", heading: "Task completed", message: "Wesley has marked the task '🍪 Eat breakfast' as completed. ", read: false, img: "/assets/images/indian-girl.webp"},
+    {name: "John", heading: "Task completed", message: "John has marked the task '🍪 Eat breakfast' as completed. ", read: false, img: "/assets/images/smile-girl.webp"},
+    {name: "Nolan", heading: "Task completed", message: "Nolan has marked the task '🍪 Eat breakfast' as completed. ", read: false, img: "/assets/images/boy-smile.webp"},
+    {name: "Layla", heading: "Task completed", message: "Layla has marked the task '🍪 Eat breakfast' as completed. ", read: true, img: "/assets/images/boy-smile.webp"},
+    {name: "Jane", heading: "Task completed", message: "Jane has marked the task '🍪 Eat breakfast' as completed. ", read: true, img: "/assets/images/girl-smile.webp"},
+    {name: "Wesley", heading: "Task completed", message: "Wesley has marked the task '🍪 Eat breakfast' as completed. ", read: true, img: "/assets/images/indian-girl.webp"},
+    {name: "John", heading: "Task completed", message: "John has marked the task '🍪 Eat breakfast' as completed. ", read: true, img: "/assets/images/smile-girl.webp"},
+
+];
+export const purchaseHistoryData = [
+    {
+        item: "Bag",
+        amount: "5,000",
+        status: "approved",
+        itemImg: "/assets/images/bag.webp"
+    },
+    {
+        item: "Sneakers",
+        amount: "10,000",
+        status: "disapproved",
+        itemImg: "/assets/images/airforce.webp"
+    },
+    {
+        item: "Guitar",
+        amount: "3,000",
+        status: "pending",
+        itemImg: "/assets/images/guitar.webp"
+    }
+]
+export const recentActivitiesData = [
+    {
+        name: "Store item purchase",
+        date: "10 hours ago",
+        amount: "-200"
+    },
+    {
+        name: "Savings funded",
+        date: "10 hours ago",
+        amount: "-20"
+    },
+    {
+        name: "Task reward",
+        date: "10 hours ago",
+        amount: "+200"
+    },
+    {
+        name: "Store item purchase",
+        date: "10 hours ago",
+        amount: "-200"
+    },
+]
+export const ongoingDoCardsData =  [
+    {
+        name: "Buy a Bicycle",
+        itemImg: "/assets/images/bike.webp",
+        points: 300,
+        target: 400,
+        profileImage: "/assets/images/boy-smile.webp",
+    },
+    {
+        name: "December Shoes",
+        itemImg: "/assets/images/airforce.webp",
+        points: 150,
+        target: 400,
+        profileImage: "/assets/images/boy-smile.webp"
+    },
+    {
+        name: "Buy a Bag",
+        itemImg: "/assets/images/bag.webp",
+        points: 200,
+        target: 500,
+        profileImage: "/assets/images/girl-smile.webp"
+    },
+    {
+        name: "Buy a Guitar",
+        itemImg: "/assets/images/guitar.webp",
+        points: 270,
+        target: 600,
+        profileImage: "/assets/images/boy-smile.webp"
+    },
+];
+export const mandatoryDoCardsData =  [
+    {
+        name: "Buy a Bicycle",
+        itemImg: "/assets/images/bike.webp",
+        points: 300,
+        target: 400,
+        profileImage: "/assets/images/boy-smile.webp",
+        isMandatory: true
+    },
+    {
+        name: "December Shoes",
+        itemImg: "/assets/images/airforce.webp",
+        points: 150,
+        target: 400,
+        profileImage: "/assets/images/boy-smile.webp",
+        isMandatory: true
+    },
+    {
+        name: "Buy a Bag",
+        itemImg: "/assets/images/bag.webp",
+        points: 200,
+        target: 500,
+        profileImage: "/assets/images/girl-smile.webp",
+        isMandatory: true
+    }
+];
+export const completedDoCardsData =  [
+    {
+        name: "Buy a Ball",
+        itemImg: "/assets/images/basketball.webp",
+        points: 400,
+        target: 400,
+        profileImage: "/assets/images/boy-smile.webp",
+        isCompleted: true
+    },
+    {
+        name: "December Shoes",
+        itemImg: "/assets/images/airforce.webp",
+        points: 400,
+        target: 400,
+        profileImage: "/assets/images/indian-girl.webp",
+        isCompleted: true
+    },
+    {
+        name: "Buy a Guitar",
+        itemImg: "/assets/images/guitar.webp",
+        points: 300,
+        target: 300,
+        profileImage: "/assets/images/smile-girl.webp",
+        isCompleted: true
+    },
+    {
+        name: "Buy a Guitar",
+        itemImg: "/assets/images/guitar.webp",
+        points: 300,
+        target: 300,
+        profileImage: "/assets/images/smile-girl.webp",
+        isCompleted: true
+    },
+    {
+        name: "Buy a Guitar",
+        itemImg: "/assets/images/guitar.webp",
+        points: 300,
+        target: 300,
+        profileImage: "/assets/images/smile-girl.webp",
+        isCompleted: true
+    },
+
+];
+export const storeCategoriesData = [
+    {
+        category: "wears",
+        img: "/assets/images/wears.png",
+    },
+    {
+        category: "gadgets",
+        img: "/assets/images/gadgets.png",
+    },
+    {
+        category: "school",
+        img: "/assets/images/school.png",
+    },
+    {
+        category: "birthdays",
+        img: "/assets/images/parties.png",
+    },
+    {
+        category: "toys",
+        img: "/assets/images/toys.png",
+    },
+    {
+        category: "dress",
+        img: "/assets/images/dress.png",
+    },
+    {
+        category: "challenges",
+        img: "/assets/images/challenges.png",
+    },
+    {
+        category: "others",
+        img: "/assets/images/others.png"
+    }
+];
